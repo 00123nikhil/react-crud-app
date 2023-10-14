effa16f6a3d1a1941b6e9edd10aaab8810653e38
Nikhil Borse <nikhilrb13@gmail.com>
2023-10-14 20:01:26 +0530

    client for frontend

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 const PORT = process.env.PORT || 8000;
 
@@ -22,13 +23,71 @@ const schemaData = mongoose.Schema(
 
 const userModel = mongoose.model("user", schemaData);
 
+// read data
+//http://localhost:8000
 app.get("/", async (req, res) => {
   const data = await userModel.find({});
   res.json({ success: true, data: data });
 });
 
-app.post("/create", (req, res) => {
+// create data in app
+// http://localhost:8000/create
+/**
+ * create data
+ * {
+ * name
+ * email
+ * mobileno
+ * }
+ */
+app.post("/create", async (req, res) => {
   console.log(req.body);
+  const data = new userModel(req.body);
+  await data.save();
+  res.send({
+    success: true,
+    messsage: "data saved",
+    data: data,
+  });
+});
+
+// upadate data in app
+//localhost:8000/update
+/**
+ * {
+ * id:""
+ * name:""
+ * email:""
+ * mobile:
+ * }
+ */
+http: app.put("/update", async (req, res) => {
+  console.log(req.body);
+  const { id, ...rest } = req.body;
+
+  console.log(rest);
+  const data = await userModel.updateOne({ _id: req.body.id }, rest);
+  res.send({
+    success: true,
+    messsage: "data updated successfully",
+    data: data,
+  });
+});
+
+// delete data
+// http://localhost:8000/delete
+/**
+ * 
+ */
+app.delete("/delete/:id", async (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const data = await userModel.deleteOne({ _id: id });
+  res.send({
+    success: true,
+    message: "data delete successfully",
+    data: data,
+  });
 });
 
 // database connection
